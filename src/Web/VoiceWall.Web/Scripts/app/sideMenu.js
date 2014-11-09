b4e4800fafdac76e2f5a137381480fa410c2f396
fast4y <dzhenko@gmail.com>
2014-11-09 13:52:54 +0200

    Added side menu which hides when on top and removed standard nav when not on top

--- src/Web/VoiceWall.Web/Scripts/app/sideMenu.js
+++ src/Web/VoiceWall.Web/Scripts/app/sideMenu.js
@@ -11,20 +11,41 @@
         $("#sidebar-wrapper").toggleClass("active");
     });
 
-    // Scrolls to the selected menu item on the page
-    $(function () {
-        $('a[href*=#]:not([href=#])').click(function () {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+    var windowIsScrooledTop = true;
+    $(window).scroll(function () {
+        if ($(this).scrollTop() == 0) {
+            // top reached
+            windowIsScrooledTop = true;
 
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
+            $("#tiny-navigation-container").hide();
+            $("#top-navigation-container").show();
+
+        }
+        else if (windowIsScrooledTop) {
+            // we move down from the top
+            windowIsScrooledTop = false;
+
+            // animation magic
+            $("#tiny-navigation-container").show();
+            $("#top-navigation-container").hide();
+        }
     });
+
+    // Scrolls to the selected menu item on the page
+    // breaks bootstraps modal window nav
+    //$(function () {
+    //    $('a[href*=#]:not([href=#])').click(function () {
+    //        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+    //            var target = $(this.hash);
+    //            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //            if (target.length) {
+    //                $('html,body').animate({
+    //                    scrollTop: target.offset().top
+    //                }, 1000);
+    //                return false;
+    //            }
+    //        }
+    //    });
+    //});
 });
